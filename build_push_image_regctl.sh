8b5331c6fdd2f5238344d8e2af6bb78755da1cea
Andrei Aaron <andaaron@cisco.com>
2023-03-09 21:31:44 +0000

    feat(metadata): store uploaded image metadata in a json file
    Signed-off-by: Andrei Aaron <andaaron@cisco.com>

--- build_push_image_regctl.sh
+++ build_push_image_regctl.sh
@@ -13,8 +13,10 @@ REGISTRY=$1
 image=$2
 tag=$3
 COSIGN_PASSWORD=$4
-USERNAME=$5
-PASSWORD=$6
+metafile=$5
+multiarch=$6
+USERNAME=$7
+PASSWORD=$8
 
 function verify_prerequisites {
     if [ ! command -v regctl ] &>/dev/null; then
@@ -29,6 +31,11 @@ function verify_prerequisites {
 
     if [ ! command -v cosign ] &>/dev/null; then
         echo "you need to install cosign as a prerequisite" >&3
+        return 1
+    fi
+
+    if [ ! command -v jq ] &>/dev/null; then
+        echo "you need to install jq as a prerequisite" >&3
         return 1
     fi
 
@@ -48,7 +55,9 @@ function verify_prerequisites {
 verify_prerequisites
 
 repo=$(cat ${DOCKER_DOCS_DIR}/${image}/github-repo)
-logo=$(base64 -w 0 ${DOCKER_DOCS_DIR}/${image}/logo.png)
+description="$(cat ${DOCKER_DOCS_DIR}/${image}/README-short.txt)"
+license="$(cat ${DOCKER_DOCS_DIR}/${image}/license.md)"
+vendor="$(cat ${DOCKER_DOCS_DIR}/${image}/maintainer.md)"
 echo ${repo}
 sed -i "s|%%GITHUB-REPO%%|${repo}|g" ${DOCKER_DOCS_DIR}/${image}/maintainer.md
 sed -i "s|%%IMAGE%%|${image}|g" ${DOCKER_DOCS_DIR}/${image}/content.md
@@ -57,18 +66,22 @@ doc=$(cat ${DOCKER_DOCS_DIR}/${image}/content.md)
 local_image_ref_skopeo=oci:${IMAGES_DIR}:${image}
 local_image_ref_regtl=ocidir://${IMAGES_DIR}:${image}
 
+multiarch_arg=""
+if [ ! -z "${multiarch}" ]; then
+    multiarch_arg="--multi-arch=${multiarch}"
+fi
+
 # Copy image in local oci layout
-skopeo --insecure-policy copy --format=oci --multi-arch=all docker://${image}:${tag} ${local_image_ref_skopeo}
+skopeo --insecure-policy copy --format=oci ${multiarch_arg} docker://${image}:${tag} ${local_image_ref_skopeo}
 
 # Mofify image in local oci layout and update the old reference to point to the new index
 regctl image mod --replace --annotation org.opencontainers.image.title=${image} ${local_image_ref_regtl}
-regctl image mod --replace --annotation com.zot.logo=${logo} ${local_image_ref_regtl}
-regctl image mod --replace --annotation org.opencontainers.image.description="$(cat ${DOCKER_DOCS_DIR}/${image}/README-short.txt)" ${local_image_ref_regtl}
+regctl image mod --replace --annotation org.opencontainers.image.description="${description}" ${local_image_ref_regtl}
 regctl image mod --replace --annotation org.opencontainers.image.url=${repo} ${local_image_ref_regtl}
 regctl image mod --replace --annotation org.opencontainers.image.source=${repo} ${local_image_ref_regtl}
-regctl image mod --replace --annotation org.opencontainers.image.licenses="$(cat ${DOCKER_DOCS_DIR}/${image}/license.md)" ${local_image_ref_regtl}
-regctl image mod --replace --annotation org.opencontainers.image.vendor="$(cat ${DOCKER_DOCS_DIR}/${image}/maintainer.md)"  ${local_image_ref_regtl}
-regctl image mod --replace --annotation org.opencontainers.image.documentation="$(cat ${DOCKER_DOCS_DIR}/${image}/README-short.txt)" ${local_image_ref_regtl}
+regctl image mod --replace --annotation org.opencontainers.image.licenses="${license}" ${local_image_ref_regtl}
+regctl image mod --replace --annotation org.opencontainers.image.vendor="${vendor}" ${local_image_ref_regtl}
+regctl image mod --replace --annotation org.opencontainers.image.documentation="${description}" ${local_image_ref_regtl}
 
 credentials_args=""
 if [ ! -z "${USERNAME}" ]; then
@@ -76,7 +89,21 @@ if [ ! -z "${USERNAME}" ]; then
 fi
 
 # Upload image to target registry
-skopeo copy --multi-arch=all --dest-tls-verify=false ${credentials_args} ${local_image_ref_skopeo} docker://${REGISTRY}/${image}:${tag}
+skopeo copy --dest-tls-verify=false ${multiarch_arg} ${credentials_args} ${local_image_ref_skopeo} docker://${REGISTRY}/${image}:${tag}
 
 # Sign new updated image
 COSIGN_PASSWORD=${COSIGN_PASSWORD} cosign sign ${REGISTRY}/${image}:${tag} --key ${COSIGN_KEY_PATH} --allow-insecure-registry
+
+details=$(jq -n \
+    --arg org.opencontainers.image.title "${image}" \
+    --arg org.opencontainers.image.description " $description" \
+    --arg org.opencontainers.image.url "${repo}" \
+    --arg org.opencontainers.image.source "${repo}" \
+    --arg org.opencontainers.image.licenses "${license}" \
+    --arg org.opencontainers.image.vendor "${vendor}" \
+    --arg org.opencontainers.image.documentation "${description}" \
+    --arg multiarch "${multiarch}" \
+    '$ARGS.named'
+)
+
+jq -n --arg image "${image}" --arg tag "${tag}"  --argjson details "${details}" '.[$image][$tag]=$details' > ${metafile}
